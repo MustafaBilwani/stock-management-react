84e62bfd3ae0ccb3bad9ca14d40a7df02465bb3c
MustafaBilwani <mustafabilwani92@gmail.com>
2024-10-26 13:46:38 +0500

    history tab completed

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,8 @@ import ProductsTab from "./ProductsTab";
 import StockTab from "./StockTab";
 import HistoryTab from "./HistoryTab";
 
+var index = -1
 function App() {
-  var index = 0;
 
   function coming(){
     var comingQty = document.getElementById('comingQty').value;
@@ -26,34 +26,27 @@ function App() {
     var comingNote = document.getElementById('comingNote').value;
     var comingProduct = document.getElementById('comingProduct').value;
     var comingDate = document.getElementById('comingDate').value;
-    index += 1;
     
     var isDecimal = comingQty != Math.trunc(comingQty);
     if (comingProduct === '' || comingQty === '' || comingPrice === '' || comingDate == '') {
       alert('Incomplete details'); return false; 
     }
-    if (isDecimal || comingPrice < 1 || comingQty < 1) {
+    if (isDecimal || comingPrice < 0 || comingQty < 1) {
       alert('Incorrect details'); return false; 
     }
+    index +=1
 
-    
-    setProductStockTotal(prevState => ({
-      ...prevState,
-      [comingProduct]: (parseInt(prevState[comingProduct]) + parseInt(comingQty))
-    }));
+    var newDetail = {
+      coming: parseInt(comingQty),
+      price: parseFloat(comingPrice),
+      date: comingDate,
+      notes: comingNote,
+      qty: comingQty,
+      id: index
+    }
+    console.log('index: ' + newDetail.id)
 
-    setProductStockDetail(prev => ({
-      ...prev,
-      [comingProduct]:  [...prev[comingProduct], 
-        {
-          coming: parseInt(comingQty),
-          price: parseInt(comingPrice),
-          date: comingDate,
-          notes: comingNote,
-          id: index + 'coming',
-          initialQty: comingQty
-        }]
-    }))
+    reRunDetails(comingProduct, newDetail);
   }
 
   function going(){
@@ -65,11 +58,11 @@ function App() {
 
     var isDecimal = goingQty != Math.trunc(goingQty);
     
-    if (goingProduct === '' || goingQty === '') {
+    if (goingProduct === '' || goingPrice === '' || goingQty === '' || goingDate === '') {
       alert('Incomplete details'); 
       return false;
     }
-    if (isDecimal || goingQty < 1) {
+    if (isDecimal || goingQty < 1 || goingPrice < 0) {
       alert('Incorrect details'); 
       return false;
     }
@@ -78,17 +71,86 @@ function App() {
       return false;
     }
 
-    setProductStockTotal(prevState => ({
-      ...prevState,
-      [goingProduct]: (parseInt(prevState[goingProduct]) - parseInt(goingQty))
-    }));
+    var newDetail = {
+      going: parseInt(goingQty),
+      price: goingPrice,
+      date: goingDate,
+      notes: goingNote,
+      purchasingAmount: '',
+    }
+
+    reRunDetails(goingProduct, newDetail);
 
   }
+
+  function reRunDetails(product, newDetail){
+    
+    
+    var update = [...productStockDetail[product], newDetail]
+    var purchasingAmountArray = []
+
+    update.sort(function(a, b){return new Date(a.date) - new Date(b.date)})
+    
+    var stock = 0
+    var status = true
+    for (let element of update) {
+      stock += element.coming > 0 ? element.coming : -element.going
+
+      if (stock < 0){
+        status = false
+        alert('not enough stock at that time')
+        break;
+      }
+
+      if (element.coming > 0){
+        debugger;
+        element.qty = element.coming
+        purchasingAmountArray.push(element.id)
+        console.log(purchasingAmountArray)
+      } else {
+        debugger;
+        var remainingQty = element.going
+        var totalPrice = 0
+        element.purchasingAmount = ''
+        
+        while(remainingQty > 0){
+          let currIndex = update.findIndex(x => x.id == purchasingAmountArray[0])
+          let currDetail = update[currIndex]
+          let currQty =  Math.min(currDetail.qty, remainingQty)
+
+          element.purchasingAmount += `${currDetail.price} PKR for ${currQty} pieces <br/>`;
+          totalPrice += currQty * currDetail.price
+          element.profit = (totalPrice / element.going - element.price).toFixed(2)
+
+          currDetail.qty -= currQty
+          remainingQty -= currQty
+
+          if(currDetail.qty == 0){
+            purchasingAmountArray.splice(0,1)
+          }
+        
+        }
+      }
+    }
+
+    if(status != false){
+      setProductStockDetail(prev => ({
+        ...prev,
+        [product]: [...update]
+      }))
+
+      setProductStockTotal(prevState => ({
+        ...prevState,
+        [product]: stock
+      }));
+    }
+      
+    };
 
   const [products, setProducts] = React.useState([]);
   const [productStockTotal, setProductStockTotal] = React.useState({});
   const [productStockDetail, setProductStockDetail] = React.useState({});
-  const [currentProductDetail, setCurrentProductDetail] = React.useState([]);
+  const [currentProductDetail, setCurrentProductDetail] = React.useState('');
 
   const renderForm = (type, title, handleAction) => (
     <VStack
@@ -158,7 +220,7 @@ function App() {
           </TabPanel>
 
           <TabPanel>
-            <ProductsTab products={products} setProducts={setProducts} productStockTotal={productStockTotal} setProductStockTotal={setProductStockTotal} productStockDetail={productStockDetail} setProductStockDetail={setProductStockDetail} />
+            <ProductsTab setCurrentProductDetail={setCurrentProductDetail} currentProductDetail={currentProductDetail} products={products} setProducts={setProducts} productStockTotal={productStockTotal} setProductStockTotal={setProductStockTotal} productStockDetail={productStockDetail} setProductStockDetail={setProductStockDetail} />
           </TabPanel>
         </TabPanels>
       </Tabs>
--- src/HistoryTab.jsx
+++ src/HistoryTab.jsx
@@ -1,66 +1,72 @@
-import { Center, Select, Table, TableContainer, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
+import { Center, Select, Table, TableContainer, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
 import React, { useEffect } from "react";
 
 function HistoryTab({products, productStockDetail, productStockTotal, setCurrentProductDetail, currentProductDetail}){
   return(
     <>
     
-    <Select onChange={(e) => {setCurrentProductDetail(productStockDetail[e.target.value])}} defaultValue="">
+    <Select onChange={(e) => {setCurrentProductDetail(e.target.value)}} defaultValue="" mb={3}>
       <option value='' disabled hidden>Select an option</option>
       {products.map((x, index) => {
         return(
-          <option value={x} >{x}</option> 
+          <option key={index} value={x} >{x}</option> 
         )
       })}
     </Select>
-    {products.length > 0 ?(
-      <TableContainer
-      border="1px"
-      borderColor="gray.300"
-      borderRadius="md"
-      p={4}
-      shadow="sm"
-      bg="white"
-    >
-      <Table variant="simple" size="md">
-        <Thead>
-          <Tr>
-            <Th borderBottom="2px solid" borderColor="gray.300">
-              Date
-            </Th>
-            <Th borderBottom="2px solid" borderColor="gray.300">
-              Notes
-            </Th>
-            <Th borderBottom="2px solid" borderColor="gray.300">
-              Coming
-            </Th>
-            <Th borderBottom="2px solid" borderColor="gray.300">
-              Going
-            </Th>
-            <Th borderBottom="2px solid" borderColor="gray.300">
-              Price
-            </Th>
-            <Th borderBottom="2px solid" borderColor="gray.300">
-              Purchasing Amount
-            </Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {currentProductDetail.map((x, index) => (
-            <Tr key={index} borderBottom="2px solid" borderColor="gray.200">
-              <Td fontWeight="medium">{x.date}</Td>
-              <Td>{x.notes}</Td>
-              <Td>{x.coming}</Td>
-              <Td>{x.going || ''}</Td>
-              <Td>{x.price}</Td>
-              <Td>{x.purchasingAmount}</Td>
-            </Tr>
-          ))}
-        </Tbody>
-      </Table>
-    </TableContainer>
-    ):(
+    {products.length == 0 ?(
       <Center>No Products yet</Center>
+    ): currentProductDetail == '' ?(
+      <Center>Select Product to view Details</Center>
+    ):(
+      <TableContainer
+        border="1px"
+        borderColor="gray.300"
+        borderRadius="md"
+        p={4}
+        shadow="sm"
+        bg="white"
+      >
+        <Table variant="simple" size="md">
+          <Thead>
+            <Tr>
+              <Th borderBottom="2px solid" borderColor="gray.300">
+                Date
+              </Th>
+              <Th borderBottom="2px solid" borderColor="gray.300">
+                Notes
+              </Th>
+              <Th borderBottom="2px solid" borderColor="gray.300">
+                Coming
+              </Th>
+              <Th borderBottom="2px solid" borderColor="gray.300">
+                Going
+              </Th>
+              <Th borderBottom="2px solid" borderColor="gray.300">
+                Price
+              </Th>
+              <Th borderBottom="2px solid" borderColor="gray.300">
+                Purchasing Amount
+              </Th>
+              <Th borderBottom="2px solid" borderColor="gray.300">
+                Profit
+              </Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {productStockDetail[currentProductDetail].map((x, index) => (
+              <Tr key={index} borderBottom="2px solid" borderColor="gray.200">
+                <Td fontWeight="medium">{x.date}</Td>
+                <Td>{x.notes}</Td>
+                <Td><Text display={ x.coming > x.qty ? "inline" : 'none'} decoration={"line-through"}>{x.coming}</Text> {x.qty}</Td>
+                <Td>{x.going}</Td>
+                <Td>{x.price}</Td>
+                <Td dangerouslySetInnerHTML={{ __html: x.purchasingAmount }} />
+                <Td>{x.profit}</Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
     )
     }
     </>
